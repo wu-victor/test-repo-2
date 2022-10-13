ad622e82329fffd7a77f1476c11c4e32203c1167
Victor Wu <wu-victor@github.com>
2022-10-13 18:18:28 -0400

    Clean up console logs

--- index.js
+++ index.js
@@ -2,17 +2,10 @@ const github = require('@actions/github');
 const core = require('@actions/core');
 const axios = require('axios');
 
-console.log('hello from inside the action on a branch and now a PR');
-
 const { context } = github;
 const { payload } = context;
 
-const { repository } = payload;
-const { pulls_url } = repository;
-
 const { commits } = payload;
-
-console.log({ pulls_url, commits, payload });
 
 const token = core.getInput('token');
 
