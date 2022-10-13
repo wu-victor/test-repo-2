5c17d640c3fb1858e64b33ce83650aa47d185c77
Victor Wu <wu-victor@github.com>
2022-10-13 16:28:03 -0400

    updatew

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const github = require('@actions/github');
 
-console.log('hello from inside the action on a branch');
+console.log('hello from inside the action on a branch and now a PR');
 
 const { context } = github;
 const { payload } = context;
