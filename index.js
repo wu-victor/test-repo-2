ebffa63fa32f31d1d9f3f18089e483cf5bc3b94e
Victor Wu <wu-victor@github.com>
2022-10-13 16:22:46 -0400

    hello

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const core = require('@actions/core');
 const github = require('@actions/github');
 
 console.log('hello from inside the action');
 console.log({ commits: github.context.payload.commits });
+
+
+const { context } = github;
+const { payload } = context;
+const { commits } = payload;
+
+console.log({ payload });
