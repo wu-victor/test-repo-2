e2228d1c711eb048def09c9114b32c351effb519
Victor Wu <wu-victor@github.com>
2022-10-13 18:14:22 -0400

    updates

--- index.js
+++ index.js
@@ -16,16 +16,24 @@ console.log({ pulls_url, commits, payload });
 
 const token = core.getInput('token');
 
+let skip = false;
+for (const commit of commits) {
+  const { message } = commit;
+  if (message.toLowerCase().includes('skip')) {
+    skip = true;
+    break;
+  }
+}
 
-(async () => {  
-  const response = await axios({
-    method: 'POST',
-    headers: { Authorization: `Bearer ${token}` },
-    url: 'https://api.github.com/repos/wu-victor/test-repo-2/issues/1/comments', // Regular comments for PRs use this endpoint
-    data: {
-      'body': 'This is a comment!'
-    }
-  });
-  const { data } = response;
-  console.log({ data })
-})();
+if (!skip) {
+  (async () => {  
+    await axios({
+      method: 'POST',
+      headers: { Authorization: `Bearer ${token}` },
+      url: 'https://api.github.com/repos/wu-victor/test-repo-2/issues/1/comments', // Regular comments for PRs use this endpoint
+      data: {
+        'body': 'If you don\'t need to run CI, add [ci skip] to your commit message in your next push'
+      }
+    });
+  })();
+}
