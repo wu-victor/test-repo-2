e017cabb44376bd14932802a8c26464343d994c5
Victor Wu <wu-victor@github.com>
2022-10-13 16:03:08 -0400

    push

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 const core = require("@actions/core");
-const fs = require("fs/promises");
-const axios = require("axios");
 
 const category = core.getInput('category') || 'inspire';
 const readme_path = core.getInput('readme_path') || 'README.md';
 
-(async () => {
-  try {
-    // Fetch the quote from API
-    const { data } = await axios.get(
-      `https://quotes.rest/qod?category=${category}`
-    );
-
-    let qotd = data.contents.quotes[0].quote;
-    console.log({ qotd })
-    
-  } catch (error) {
-    console.log(error.message);
-  }
-})();
+console.log('hello from inside the action');
