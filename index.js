4e10a25f3562e28b0fa64363595cde247b0aca16
Victor Wu <wu-victor@github.com>
2022-10-13 16:12:58 -0400

    updates

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 const category = core.getInput('category') || 'inspire';
 const readme_path = core.getInput('readme_path') || 'README.md';
