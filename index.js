62e70b31e71908563595bc8cb4582ecec66bfe11
Victor Wu <wu-victor@github.com>
2022-10-13 16:09:52 -0400

    start

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const core = require("@actions/core");
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 const category = core.getInput('category') || 'inspire';
 const readme_path = core.getInput('readme_path') || 'README.md';
 
 console.log('hello from inside the action');
+console.log({ payload: github.context.payload });
